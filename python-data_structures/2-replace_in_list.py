c8cc15b890e0638aa3dcaf3607eb03bf034e2748
Ayen-Guech <a.guech@alustudent.com>
2023-07-16 09:31:45 -0700

    task 2

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return the original list
+    if idx >= len(my_list):
+        return the original list
+    return my_list[idx]
