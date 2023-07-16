69c77a017c996b7b476bd96207a7e403ee1f6042
Ayen-Guech <a.guech@alustudent.com>
2023-07-16 09:35:07 -0700

    task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return the original list
+        return the Original list
     if idx >= len(my_list):
-        return the original list
+        return the Original list
     return my_list[idx]
