36c128975550adf3b79c78fa9025de80c34912fd
Ayen-Guech <a.guech@alustudent.com>
2023-07-17 07:59:53 -0700

    task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+# 9-max_integer.py
+
+
 def max_integer(my_list=[]):
-  if my_list == []:
-    return None
-  max_value = my_list[0]
-  for element in my_list:
-    if element > max_value:
-      max_value = element
-  return max_value
+    """Find the biggest integer of a list."""
+    if len(my_list) == 0:
+        return (None)
+
+    big = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big:
+            big = my_list[i]
+
+    return (big)
