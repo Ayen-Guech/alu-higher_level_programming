0014e1d296d16a2704b67d11bf841ef220cc60c1
Ayen-Guech <a.guech@alustudent.com>
2023-08-04 00:25:33 -0700

    task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,7 @@
--- Lists all cities of CA in the database hbtn_0d_usa.
--- Results are ordered by ascending cities.id.
-SELECT `id`, `name`
-  FROM `cities`
- WHERE `state_id` IN
-       (SELECT `id`
-	  FROM `states`
-	 WHERE `name` = "California")
- ORDER BY `id`;
+-- script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
+       	state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id),
+	name VARCHAR(256) NOT NULL
+);
